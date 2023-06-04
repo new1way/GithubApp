a6efcad499c02b632056925f0fac0e05afa6995b
RaoulFOS <megalpixel8991@gmail.com>
2023-06-04 17:55:47 +0200

    mon premier jeu

--- main.py
+++ main.py
@@ -1,13 +1,71 @@
+import pygame
+import random
 
+# Initialisation de Pygame
+pygame.init()
 
+# Définition de la taille de la fenêtre du jeu
+largeur = 800
+hauteur = 600
+fenetre = pygame.display.set_mode((largeur, hauteur))
+pygame.display.set_caption("Jeu d'évitement")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Couleurs
+blanc = (255, 255, 255)
+noir = (0, 0, 0)
 
+# Paramètres du personnage
+perso_taille = 50
+perso_x = largeur / 2 - perso_taille / 2
+perso_y = hauteur - perso_taille - 10
+perso_vitesse = 5
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Paramètres des obstacles
+obstacle_taille = 50
+obstacle_x = random.randint(0, largeur - obstacle_taille)
+obstacle_y = -obstacle_taille
+obstacle_vitesse = 3
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Boucle principale du jeu
+running = True
+clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Déplacement du personnage
+    touches = pygame.key.get_pressed()
+    if touches[pygame.K_LEFT] and perso_x > 0:
+        perso_x -= perso_vitesse
+    if touches[pygame.K_RIGHT] and perso_x < largeur - perso_taille:
+        perso_x += perso_vitesse
+
+    # Déplacement de l'obstacle
+    obstacle_y += obstacle_vitesse
+    if obstacle_y > hauteur:
+        obstacle_x = random.randint(0, largeur - obstacle_taille)
+        obstacle_y = -obstacle_taille
+
+    # Collision entre le personnage et l'obstacle
+    if obstacle_y + obstacle_taille >= perso_y and obstacle_y <= perso_y + perso_taille:
+        if obstacle_x + obstacle_taille >= perso_x and obstacle_x <= perso_x + perso_taille:
+            running = False
+
+    # Effacer l'écran
+    fenetre.fill(blanc)
+
+    # Dessiner le personnage
+    pygame.draw.rect(fenetre, noir, (perso_x, perso_y, perso_taille, perso_taille))
+
+    # Dessiner l'obstacle
+    pygame.draw.rect(fenetre, noir, (obstacle_x, obstacle_y, obstacle_taille, obstacle_taille))
+
+    # Mettre à jour l'affichage
+    pygame.display.update()
+    clock.tick(60)
+
+# Quitter le jeu
+pygame.quit()
+
